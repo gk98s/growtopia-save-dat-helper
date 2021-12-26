de53d3c157760c7e3dfc30ca481f9b7603da5bab
GK98 <GK98@DEVICE>
2021-12-26 19:04:56 +0300

    done 1

--- mainform.Designer.cs
+++ mainform.Designer.cs
@@ -146,6 +146,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "mainform";
             this.Text = "Save Dat Helper";
             this.Load += new System.EventHandler(this.Form1_Load);
